b24d118ce7ff5cb224dd702a7e69f638bbedbdbc
Spicysnowe <85620483+Spicysnowe@users.noreply.github.com>
2022-03-09 03:23:40 +0530

    ninth commit

--- app/src/main/java/com/example/minditup/Categories.java
+++ app/src/main/java/com/example/minditup/Categories.java
@@ -10,8 +10,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class Categories extends AppCompatActivity {
 
-    RelativeLayout depressed;
-    TextView deptext;
+    RelativeLayout depressed, stress, anxiety, sleep, angry, relation;
+    TextView deptext, strtext, anxtext, slptext, agrtext, reltext;
 
 
     @Override
@@ -22,6 +22,28 @@ public class Categories extends AppCompatActivity {
 
         depressed= findViewById(R.id.depressed);
         deptext= findViewById(R.id.deptext);
+
+        anxiety= findViewById(R.id.anxiety);
+        anxtext= findViewById(R.id.anxtext);
+
+        angry= findViewById(R.id.angry);
+        agrtext= findViewById(R.id.agrtext);
+
+        stress= findViewById(R.id.stress);
+        strtext= findViewById(R.id.strtext);
+
+        sleep= findViewById(R.id.sleep);
+        slptext= findViewById(R.id.slptext);
+
+        relation= findViewById(R.id.relation);
+        reltext= findViewById(R.id.reltext);
+
+
+
+
+
+//        OnClicks
+
         depressed.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -29,6 +51,91 @@ public class Categories extends AppCompatActivity {
                 if(detext.equals("Overcoming Depression")){
                     startActivity(new Intent(Categories.this,DepressedActivity.class));
                     deptext.setText("Overcoming Depression.");
+                }else{
+                    startActivity(new Intent(Categories.this,MainActivity.class));
+                }
+
+
+            }
+        });
+
+
+
+        anxiety.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String antext = anxtext.getText().toString();
+                if(antext.equals("Beating Anxiety")){
+                    startActivity(new Intent(Categories.this,AnxietyActivity.class));
+                    anxtext.setText("Beating Anxiety.");
+                }else{
+                    startActivity(new Intent(Categories.this,MainActivity.class));
+                }
+
+
+            }
+        });
+
+
+
+        angry.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String agtext = agrtext.getText().toString();
+                if(agtext.equals("Managing Anger")){
+                    startActivity(new Intent(Categories.this,AngryActivity.class));
+                    agrtext.setText("Managing Anger.");
+                }else{
+                    startActivity(new Intent(Categories.this,MainActivity.class));
+                }
+
+
+            }
+        });
+
+
+
+        stress.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String sttext = strtext.getText().toString();
+                if(sttext.equals("Tackling Stress")){
+                    startActivity(new Intent(Categories.this,StressActivity.class));
+                    strtext.setText("Tackling Stress.");
+                }else{
+                    startActivity(new Intent(Categories.this,MainActivity.class));
+                }
+
+
+            }
+        });
+
+
+
+        sleep.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String sltext = slptext.getText().toString();
+                if(sltext.equals("Sleeping Better")){
+                    startActivity(new Intent(Categories.this,SleepActivity.class));
+                    slptext.setText("Sleeping Better.");
+                }else{
+                    startActivity(new Intent(Categories.this,MainActivity.class));
+                }
+
+
+            }
+        });
+
+
+
+        relation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String retext = reltext.getText().toString();
+                if(retext.equals("Managing Relationships")){
+                    startActivity(new Intent(Categories.this,RelationActivity.class));
+                    reltext.setText("Managing Relationships.");
                 }else{
                     startActivity(new Intent(Categories.this,MainActivity.class));
                 }
